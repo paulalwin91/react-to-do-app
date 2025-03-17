1df3aea8bb5f4da02aaa01216d6e82c2cbfdaa0d
Paul Alwin <paulalwin91@gmail.com>
2025-03-16 22:09:02 -0400

    Handle Delete and check boxes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,70 @@
 import "./styles.css"
+import { useState } from "react"
 
 export default function App()
 {
+  const [newItem, setNewItem] = useState('')
+  const [items, setItems] = useState([])
+
+  function handleSubmit(e)
+  {
+    e.preventDefault()
+    setItems(current =>
+    {
+      return [...current, {
+        id: Date.now(),
+        title: newItem,
+        completed: false
+      }]
+    })
+    setNewItem('')
+  }
+
+  function handleToggle(id)
+  {
+    setItems(current =>
+    {
+      return current.map(item =>
+      {
+        if (item.id !== id) return item
+        return { ...item, completed: !item.completed }
+      })
+    })
+  }
+
+
+  function handleDelete(id)
+  {
+    setItems(current =>
+    {
+      return current.filter(item => item.id !== id)
+    })
+  }
+
   return (
     <>
-      <form className="new-item-form">
+      <form className="new-item-form" onSubmit={handleSubmit}>
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input type="text" id="item" />
+          <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" />
         </div>
         <button className="btn">Add</button>
       </form>
+
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            <span>Item 1</span>
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            <span>Item 2</span>
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {items.length === 0 && <b><i>No Items</i></b>}
+
+        {items.map(item =>
+        {
+          return (<li key={item.id} >
+            <label>
+              <input type="checkbox" checked={item.completed} onChange={e => handleToggle(item.id)} />
+              <span>{item.title}</span>
+            </label>
+            <button className="btn btn-danger" onClick={e => handleDelete(item.id)}>Delete</button>
+          </li>)
+        })}
       </ul>
     </>
   )
